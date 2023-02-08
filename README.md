2c727a8160e72f13c847f0f64489fb4814c44b4f
decimatedTomato <greentomatopower@gmail.com>
2023-02-08 13:29:25 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+## PURPOSE
+The purpose of this repository is to act as a basic setup for a project that will use OpenGL to render shaders in c.
+
+### Makes use of
+- GLEW x64 - dll should be in bin folder
+- GLFW x64
+
+### Requirements for compilation
+- MinGW-W64 compiler
+- GCC should be up to date
+- Libraries are .dll (MAYBE .A) for MinGW AND NOT .lib (MSVC)
--- build.sh
+++ build.sh
@@ -2,10 +2,4 @@
 
 mkdir -p bin
 cp lib/glew32.dll bin/glew32.dll
-gcc src/hello.c -o bin/hello -Iinclude -Llib -lmingw32 -lglfw3 -lopengl32 -lgdi32 -luser32 -lglew32
-
-# USING MINGW-W64
-# USING GLEW x64 - DLL MUST BE IN SAME FOLDER AS CC OUTPUT
-# USIGN GLFW x64
-# MAKE SURE LIBS ARE .DLL (MAYBE .A) FOR MINGW AND NOT .LIB (MSVC)
-# MAKE SURE GCC IS UP TO DATE
+gcc src/hello.c -o bin/hello -Iinclude -Llib -lmingw32 -lglfw3 -lopengl32 -lgdi32 -luser32 -lglew32 -Wall -Wextra
